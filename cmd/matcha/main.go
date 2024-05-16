d1d2ee5cf3daf6049c3af2e57e2f586dfa2acff5
latortuga0x71 <yourmom.com>
2024-05-16 12:40:15 -0400

    working loop

--- cmd/matcha/main.go
+++ cmd/matcha/main.go
@@ -5,13 +5,15 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"runtime"
 	"syscall"
+	"time"
 )
 
 func main() {
 	fmt.Println("Matcha")
 	// Spawn Process With Args
-	debugger()
+	debuggerLoop()
 	// Insert BreakPoints At Basic Blocks
 	// Save BreakPoints, save original bytes
 	// Continue Execution
@@ -66,29 +68,82 @@ func GetReg(pid int) syscall.PtraceRegs {
 	return reg
 }
 
-func debugger() {
-	input := "who"
-	cmd := exec.Command(input)
-	cmd.Args = []string{input}
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.SysProcAttr = &syscall.SysProcAttr{Ptrace: true}
-	err := cmd.Start()
+func GetPC(pid int) uintptr {
+	var reg syscall.PtraceRegs
+	err := syscall.PtraceGetRegs(pid, &reg)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("ERROR: GETPC -> %v", err))
+	}
+	return uintptr(reg.PC())
+}
+
+func SetPC(pid int, pc uint64) {
+	var regs syscall.PtraceRegs
+	err := syscall.PtraceGetRegs(pid, &regs)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = cmd.Wait()
-	log.Printf("State: %v\n", err)
-	log.Println("Restarting...")
-	err = syscall.PtraceCont(cmd.Process.Pid, 0)
-	if err != nil {
-		log.Panic(err)
-	}
-	var ws syscall.WaitStatus
-	_, err = syscall.Wait4(cmd.Process.Pid, &ws, syscall.WALL, nil)
+	regs.SetPC(pc)
+	err = syscall.PtraceSetRegs(pid, &regs)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("Exited: %v\n", ws.Exited())
-	log.Printf("Exit status: %v\n", ws.ExitStatus())
+}
+
+func SubRip(pid int) {
+	var regs syscall.PtraceRegs
+	err := syscall.PtraceGetRegs(pid, &regs)
+	if err != nil {
+		log.Fatal(err)
+	}
+	regs.Rip -= 1
+	err = syscall.PtraceSetRegs(pid, &regs)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func debuggerLoop() {
+	runtime.LockOSThread()
+	iterations := 0
+	devNull, _ := os.OpenFile(os.DevNull, os.O_WRONLY, 0755)
+	for {
+		input := "who"
+		cmd := exec.Command(input)
+		cmd.Args = []string{input}
+		cmd.Stdout = devNull
+		cmd.Stderr = devNull
+		cmd.SysProcAttr = &syscall.SysProcAttr{Ptrace: true}
+		err := cmd.Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = cmd.Wait()
+		pid := cmd.Process.Pid
+		log.Printf("Debugging Pid... %d", pid)
+		//log.Printf("State: %v\n", err)
+		breakPointAddress := uintptr(GetPC(pid) + 0x8)
+		originalBytes := SetBP(pid, breakPointAddress)
+		ContinueExec(pid)
+		var ws syscall.WaitStatus
+		_, err = syscall.Wait4(pid, &ws, syscall.WALL, nil)
+		if err != nil {
+			log.Println("PTRACE ERROR 1")
+			log.Fatal(err)
+		}
+		DelBP(pid, breakPointAddress, originalBytes)
+		SubRip(pid)
+		ContinueExec(pid)
+		_, err = syscall.Wait4(pid, &ws, syscall.WALL, nil)
+		if err != nil {
+			log.Println("PTRACE ERROR 2")
+			log.Fatal(err)
+		}
+		//log.Printf("Exited: %v\n", ws.Exited())
+		//log.Printf("Exit status: %v\n", ws.ExitStatus())
+		log.Printf("Iterations %v\n", iterations)
+		iterations++
+		time.Sleep(time.Millisecond * 500)
+	}
+	runtime.UnlockOSThread()
 }
